167e6a39531b0f51c68123531c1bdf0a08e3828c
Feiyu Xiang <fxiang3@uchicago.edu>
2026-01-26 15:23:20 -0600

    Fix ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,16 @@ export default [
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'commonjs'
-    },
-    env: {
-      node: true,
-      jest: true
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly'
+      }
     },
     rules: {
       'no-unused-vars': 'warn'
